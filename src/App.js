bfb378c8c3e3f24bb6227fdf89550eaee282e03a
vovacelidze <louglescom@gmail.com>
2022-09-28 17:30:58 +0300

    added loader and own useFetchHook but don't use it yet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React, {useMemo, useState} from "react";
+import React, {useEffect, useState} from "react";
 import DynamicPostList from "./components/DynamicPostList";
 import PostList from "./components/PostList";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import {useFetch} from "./hooks/useFetch";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
   const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
+  // const [fetchPosts, isLoading, postErr] = useFetch(async () => {
+  //   const posts = await PostService.getAll();
+  //   setPosts(posts);
+  // })
+  const [isLoading, setIsLoading] = useState(false);
+  
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+  
+  
+  async function fetchPosts() {
+    setIsLoading(true);
+    const posts = await PostService.getAll();
+    setPosts(posts);
+    setIsLoading(false);
+  }
   
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -29,7 +50,10 @@ function App() {
       </MyModal>
       <hr style={{margin: '15px 0'}}/>
       <PostFilter filter={filter} setFilter={setFilter}/>
-      <PostList posts={sortedAndSearchPosts} remove={removePost} title={'Posts about something'}/>
+      {isLoading
+      ? <Loader />
+      : <PostList posts={sortedAndSearchPosts} remove={removePost} title={'Posts about something'}/>
+      }
     </div>
   );
 }
