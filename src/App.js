9c5d0b7b1ee07fbfafe6397449eb97dc4f698944
vovacelidze <louglescom@gmail.com>
2022-09-28 15:53:44 +0300

    added userHooks

--- src/App.js
+++ src/App.js
@@ -4,26 +4,13 @@ import PostList from "./components/PostList";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: "About 1", body: "Hello World"},
-    {id: 2, title: "Zebra 2", body: "Its Description"},
-    {id: 3, title: "Honda 3", body: "Awesome"}
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
-  
-  const sortedPost = useMemo(() => {
-    if(filter.sort){
-      return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts])
-  
-  const sortedAndSearchPosts = useMemo( () => {
-    return sortedPost.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPost])
+  const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
   
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
